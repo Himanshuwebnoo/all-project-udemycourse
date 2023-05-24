d8394596f8d1095cbc1ad48dc6d3ced5f3093ae2
Himanshu <himanshu.khati@webnoo.com>
2023-05-24 18:12:44 +0530

    added hemet validation in ajax file upload

--- Ajaxfileupload/app.js
+++ Ajaxfileupload/app.js
@@ -1,29 +1,70 @@
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var fs          = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var fileUpload = require('express-fileupload');
+var helmet = require('helmet');
 
 var app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true,limit:'50mb',parameterLimit:50000}));
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://stackpath.bootstrapcdn.com"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+    },
+  })
+);
 
-app.use('/jquery',express.static(__dirname+'/node_modules/jquery/dist/'));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>{
-   res.render('demo');
+app.use(bodyParser.urlencoded({ extended: true, limit: '150kb', parameterLimit: 50000 }));
+
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/images', express.static(__dirname + '/images')); // Serve static files from /images directory
+
+app.use(
+  fileUpload({
+   limits: { fileSize: 200 * 1024 }, // Set the file size limit to 5KB (5 * 1024 bytes)
+  })
+);
+
+app.get('/', (req, res) => {
+  res.render('demo', { nonce: res.locals.nonce });
 });
 
-app.post('/upload',(req,res)=>{
-   var path     = __dirname+'/'+req.body.filename;
-   var image    = req.body.file;
-   var data     = image.split(',')[1];
-   fs.writeFileSync(path,data,{encoding:'base64'});
-    var temp        = fs.readFileSync(path);
-    var buff        = new Buffer(temp);
-    var base64data  = buff.toString('base64');
-    res.json({msg:'success',data:base64data});
+app.post('/upload', (req, res) => {
+  if (!req.files || !req.files.file) {
+    res.status(400).json({ msg: 'No file uploaded' });
+    return;
+  }
+
+  var uploadedFile = req.files.file;
+
+  if (uploadedFile.size > 200 * 1024) {
+   res.status(400).json({ msg: 'File size exceeds the limit of 200KB' });
+   return;
+ }
+
+  var filename = uploadedFile.name;
+  var filePath = path.join(__dirname, 'images', filename);
+
+  uploadedFile.mv(filePath, function (err) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ msg: 'Error uploading file' });
+      return;
+    }
+
+    var temp = fs.readFileSync(filePath);
+    var base64data = temp.toString('base64');
+    res.json({ msg: 'success', data: base64data, filename: filename });
+  });
 });
 
-var port  = process.env.port || 3001;
-app.listen(port,()=>console.log('server run at port '+port));
+var port = process.env.PORT || 3001;
+app.listen(port, () => console.log('Server is running on port ' + port));
